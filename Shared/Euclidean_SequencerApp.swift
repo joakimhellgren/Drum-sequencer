80f8165479142412b9b9c0366811ddc68e60f756
Joakim Hellgren <hellojoakim@gmail.com>
2021-03-04 08:34:05 +0100

    Initial Commit

---
+++ Shared/Euclidean_SequencerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Euclidean_SequencerApp.swift
+//  Shared
+//
+//  Created by Joakim Hellgren on 2021-03-04.
+//
+
+import SwiftUI
+
+@main
+struct Euclidean_SequencerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
